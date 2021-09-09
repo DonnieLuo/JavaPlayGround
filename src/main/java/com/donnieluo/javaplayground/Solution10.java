f70b4c0c0367fd4b7c9c1fa1a0ff915cc1e500dd
Donnie <815514311@qq.com>
2021-09-09 14:07:13 +0800

    10

---
+++ src/main/java/com/donnieluo/javaplayground/Solution10.java
@@ -0,0 +1,16 @@
+package com.donnieluo.javaplayground;
+
+public class Solution10 {
+    public static boolean isMatch(String s, String p) {
+        char[] cs = s.toCharArray();
+        char[] cp = p.toCharArray();
+        int sl = cs.length;
+        int pl = cp.length;
+        if (sl == 0 || pl == 0)
+            return false;
+        boolean[][] dp = new boolean[sl][pl];
+
+
+        return false;
+    }
+}
---
+++ src/test/java/com/donnieluo/javaplayground/Main.java
@@ -0,0 +1,9 @@
+package com.donnieluo.javaplayground;
+
+
+public class Test {
+    @org.junit.Test
+    public void a() {
+        System.out.println(System.currentTimeMillis());
+    }
+}
